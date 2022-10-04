6d17a2cc65d8c46ec6a48615a3ac2ad8e365d633
fefossa <fefossa@gmail.com>
2022-10-04 16:15:02 -0400

    update notebooks and add examples

--- utilitary.py
+++ utilitary.py
@@ -5,6 +5,8 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import seaborn as sns
+import os
+import easygui as eg
 
 def pic_height(figname):
     """
@@ -34,7 +36,7 @@ def fig_micrometers():
     """
     Function that asks for objective size used to acquire images with Cytation 5. The values here provided are specifically for Cytation 5 objectives - Biotek. Takes user input only.
     """
-    answer = input('Which objective size have you used? Options are 4x, 10x and 20x.')
+    answer = eg.enterbox(msg="'Which objective size have you used? Options are 4x, 10x and 20x.'", default='', strip=True)
     objective_conversion = 0
     if answer == '4x':
         objective_conversion = 1.611928
@@ -54,8 +56,9 @@ def open_layout():
     """
     Have a folder with the date of the assay (YYYYMMDD), and a csv file with metadata of the assay based on the date of the assay (YYYYMMDD) + "metadata.csv". Or provide path within the name of the file: for example, "20181102/20181102_metadata.csv"  
     """
-    date = input('Which is the date of your experiment (YYYYMMD)? Provide the path if different from YYYYMMDD')
-    df = pd.read_csv(date + '/layout_' + date + '.csv', sep = ',')
+    path_files = eg.fileopenbox(msg="CHOOSE LAYOUT FILE", default='*', filetypes=None, multiple=False)
+    date = eg.enterbox(msg="PROVIDE DATE WHEN THE ASSAY WAS PERFORMED", default='', strip=True)
+    df = pd.read_csv(path_files, sep = ',')
     df.head(20)
     return df, date
 
@@ -90,13 +93,13 @@ def time_velocity(layout, index, pic_total_area):
     layout.at[index,'Time'] = xtime
     return xtime
 
-def open_df(well, date, pic_total_area, terminator = "_data", area_column = "Area (pixel^2)"):
+def open_df(well, pic_total_area, suffix = "_data", area_column = "Area (pixel^2)", path_files = 'Example'):
     """
     Open dataframe, create cell-covered column, and return df.
-    terminator = "_data" is standard, change if different.
+    suffix = "_data" is standard, change if different.
     area_column = "Area (pixel^2) standard. Provide the column that has the scratch measured area in pixels. 
     """
-    df = pd.read_csv(date + '/' + well + terminator + ".csv", sep="\t|:|;", encoding = "unicode_escape", engine = "python")
+    df = pd.read_csv(path_files + '/' + well + suffix + ".csv", sep="\t|:|;", encoding = "unicode_escape", engine = "python")
     cell_covered = pic_total_area - df[area_column]
     df["cell covered"] = cell_covered
     return df
@@ -123,7 +126,9 @@ def fit_linear_regression(df, layout, begin, end, index, time_column = "Time (mi
     time_column = provide which is the name of the column containing the information about time between pictures. 
     """
     y = df['cell covered'][begin:end].to_numpy().reshape(-1, 1)
+    # print(y, 'y')
     X = df[time_column][begin:end].to_numpy().reshape(-1, 1)
+    # print(X, 'x')
     reg = LinearRegression().fit(X, y) #create and fit linear regression function
     #df.at: Access a single value for a row/column label pair, and save the variables into the df layout
     layout.at[index,'Slope'] = reg.coef_ #slope
@@ -139,7 +144,13 @@ def calculate_fit(df, layout, begin, end, index, time_column = "Time (min)"):
     y_fit = ((layout.at[index,'Slope'])*x_fit) + layout.at[index,'Intercept']
     return x_fit, y_fit
     
-def plot(df, layout, begin, end, index, date, well, time, pic_total_area, x_fit, y_fit, time_column = "Time (min)"):
+def output_folder(path_files = 'Example'):
+    directory = "output"
+    path = os.path.join(path_files, directory)
+    os.makedirs(path, exist_ok=True)
+    return path
+
+def plot(df, layout, begin, end, index, date, well, time, pic_total_area, x_fit, y_fit, time_column = "Time (min)", output_path = 'Example'):
     """
     Define variables and where to plot each information, then generate a plot for each well.
     """
@@ -159,7 +170,7 @@ def plot(df, layout, begin, end, index, date, well, time, pic_total_area, x_fit,
     plt.xlabel('Time(min)')
     plt.ylabel('Cell-covered area')
     plt.title(layout['Cell'][index] + " " + layout['Treatment'][index])
-    plt.savefig(date + '/layout_' + well + '.png', dpi = 150, bbox_inches='tight')
+    plt.savefig(output_path + '/layout_' + well + '.png', dpi = 150, bbox_inches='tight')
     plt.show()
     plt.cla()
     plt.clf()
@@ -180,7 +191,7 @@ def change_end_fit(layout):
     for end in user_list:
         layout.at[end,"End"] = change_to
         
-def plot_all(layout, hueorder, date, xaxis = "Cell", yaxis = "Velocity", ylimit = (0,1), ylabel = "Cell migration velocity \u03bcm/min"):
+def plot_all(layout, hueorder, date, xaxis = "Cell", yaxis = "Velocity", ylimit = (0,1), ylabel = "Cell migration velocity \u03bcm/min", output_path = 'Example'):
     """
     Plot all the datapoints belonging to that assay. 
     xaxis and yaxis = provide which columns to plot
@@ -191,25 +202,25 @@ def plot_all(layout, hueorder, date, xaxis = "Cell", yaxis = "Velocity", ylimit 
     layout.columns = layout.columns.str.replace(' ', '')
     #hueorder = sorted(list(layout['Treatment'].unique()))  
     sns.set(font_scale=2)
-    g = sns.catplot(x = xaxis, y = yaxis, kind = "box", hue = ("Treatment"), height = 10, aspect = 1.5, hue_order = hueorder, data = layout, palette = 'husl') 
+    g = sns.catplot(x = xaxis, y = yaxis, kind = "violin", hue = ("Treatment"), height = 10, aspect = 1.5, hue_order = hueorder, data = layout, palette = 'husl') 
     plt.ylim(ylimit)
     plt.xlabel("Cells", fontsize=20)
     plt.ylabel(ylabel, fontsize=20)
     g.fig.suptitle(ylabel,y=1.1,fontsize=25)
     g._legend.set_title("Treatments")
     plt.show()
-    g.savefig(date + "/" + yaxis + date + '.tiff')
-    g.savefig(date + "/" + yaxis + date + '.svg')
+    g.savefig(output_path + "/" + yaxis + date + '.tiff')
+    g.savefig(output_path + "/" + yaxis + date + '.svg')
     sns.reset_orig()
      
 
-def import_results():
+def import_results(output_path = 'Example'):
 
     """
     Import results calculated during processing scratch assay to plot in another notebook.
     """
     date = input('Which is the date of your experiment (YYYYMMD)? Provide the path if different from YYYYMMDD')
-    df = pd.read_csv(date + '/Results_' + date + '.csv', sep = ',')
+    df = pd.read_csv(output_path + '/Results_' + date + '.csv', sep = ',')
     df.head(20)
     return df, date
     
